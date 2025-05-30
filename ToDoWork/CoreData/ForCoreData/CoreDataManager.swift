2eedfb8ef651fe8b92ff7eb5c71074a7b863f598
Artem Tarasov <artemdrolllted@gmail.com>
2025-05-30 14:07:43 +0300

    Step 2 - add FAUD for notes in CoreDataManager

--- ToDoWork/CoreData/ForCoreData/CoreDataManager.swift
+++ ToDoWork/CoreData/ForCoreData/CoreDataManager.swift
@@ -12,6 +12,8 @@ final class CoreDataManager {
     
     static let shared = CoreDataManager()
     
+    var notes: [Note] = []
+    
     private init(){}
     
     lazy var persistentContainer: NSPersistentContainer = {
@@ -36,6 +38,85 @@ final class CoreDataManager {
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
+    }
+    
+    //MARK: - Fetch, Add, Update, Delete notes
+    
+    func fetchNotes() {
+        let req = Note.fetchRequest()
+        if let notes = try? persistentContainer.viewContext.fetch(req) {
+            
+        }
+    }
+    
+    func addOrUpdateNote(note: Note) throws {
+        let context = persistentContainer.viewContext
+        try context.performAndWait {
+            if note.objectID.isTemporaryID {
+                do {
+                    try context.save()
+                } catch {
+                    context.rollback()
+                    throw error
+                }
+            } else {
+                if let existingNote = try? getNoteById(id: note.id!) {
+                    existingNote.textNotes = note.textNotes
+                    existingNote.titleNotes = note.titleNotes
+                    do {
+                        try context.save()
+                    } catch {
+                        context.rollback()
+                        throw error
+                    }
+                }
+            }
+        }
+    }
+    
+    func getNotes() throws {
+         let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+         let context = persistentContainer.viewContext
+         
+         context.performAndWait {
+             do {
+                 notes = try context.fetch(fetchRequest)
+             } catch {
+                 fatalError("Failed to fetch notes: \(error)")
+             }
+         }
+     }
+    
+    func deleteNotes(id: UUID) throws {
+        let context = persistentContainer.viewContext
+        
+        try context.performAndWait {
+            guard let note = try getNoteById(id: id) else {return}
+            context.delete(note)
+            do{
+                try context.save()
+                try getNotes()
+            } catch {
+                context.rollback()
+                throw error
+            }
+        }
+    }
+    
+    func getNoteById(id: UUID) throws -> Note? {
+        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+        
+        let context = persistentContainer.viewContext
+        var note: Note? = nil
+        context.performAndWait {
+            do {
+                note = try context.fetch(fetchRequest).first
+            } catch {
+                fatalError("Failed to fetch note by id: \(error)")
+            }
+        }
+        return note
     }
     
     
